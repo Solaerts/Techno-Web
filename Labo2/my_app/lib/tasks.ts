92f3887153991287335e46c7c78848c0811676db
Solaerts <willaertsnicolas@gmail.com>
2025-12-01 10:16:44 +0100

    data base ajoutée

--- Labo2/my_app/lib/tasks.ts
+++ Labo2/my_app/lib/tasks.ts
@@ -1,5 +1,8 @@
 'use server'
 
+import { db } from '@/db'
+import { tasksTable } from '@/db/schema'
+import { eq } from 'drizzle-orm'
 import { headers } from 'next/headers'
 import { redirect } from 'next/navigation'
 
@@ -21,4 +24,32 @@ export async function addContact(form: FormData) {
     })
     //recharger la page
     redirect((await headers()).get('referer') ?? '/')
+}
+
+export async function getTasks() {
+  return await db.select().from(tasksTable)
+}
+
+export async function addTask(form: FormData) {
+  await db.insert(tasksTable).values({
+    title: String(form.get('title')),
+    done: false,
+  })
+  redirect((await headers()).get('referer') ?? '/')
+}
+
+export async function editTask(form: FormData) {
+  await db
+    .update(tasksTable)
+    .set({
+      title: String(form.get('title')),
+      done: form.get('done') === 'on',
+    })
+    .where(eq(tasksTable.id, String(form.get('id'))))
+  redirect((await headers()).get('referer') ?? '/')
+}
+
+export async function removeTask(id: string) {
+  await db.delete(tasksTable).where(eq(tasksTable.id, id))
+  redirect((await headers()).get('referer') ?? '/')
 }
